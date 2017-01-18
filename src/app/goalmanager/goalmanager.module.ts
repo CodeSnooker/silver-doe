ec0e8e31f09106696a3668ebd1b16df146757d03
Paras Mendiratta <paras.mendiratta@timesinternet.in>
2017-01-18 20:07:52 +0530

    Added sorting for goals

--- src/app/goalmanager/goalmanager.module.ts
+++ src/app/goalmanager/goalmanager.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SDCommonModule } from './../common/common.module';
 import { MaterialModule } from '@angular/material';
+import { DndModule } from 'ng2-dnd';
 
 import { GoalModule } from './goal/goal.module';
 import { GoalManagerComponent } from './goalmanager.component';
 
 @NgModule({
-    imports: [ SDCommonModule, MaterialModule.forRoot(), BrowserModule, GoalModule ],
+    imports: [ SDCommonModule, MaterialModule.forRoot(), BrowserModule, GoalModule, DndModule.forRoot() ],
     declarations: [ GoalManagerComponent ],
     exports: [ GoalManagerComponent ]
 })
