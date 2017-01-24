757301f9dcc1af317e91968b17eae40c5f3749ba
Paras Mendiratta <code.snooker@gmail.com>
2017-01-24 06:22:43 +0530

    Sort Task in the goal editor (Only pending tasks)

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { SDCommonModule } from './../common/common.module';
 import { PlannerModule } from './../planner/planner.module';
+import { DndModule } from 'ng2-dnd';
 
 import { EditorComponent } from './editor.component';
 import { TaskService } from './../tasks/task/task.service';
 
 @NgModule({
-    imports: [PlannerModule.forRoot(), SDCommonModule.forRoot()],
+    imports: [PlannerModule.forRoot(), SDCommonModule.forRoot(), DndModule.forRoot()],
     declarations: [ EditorComponent ],
     exports: [ EditorComponent ],
     providers: [ TaskService ]
